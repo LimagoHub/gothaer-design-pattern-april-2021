0ee049aa1db1f9b153caf7c09fbe1182d5bb8371
limagoHub <limagoHub@limago.de>
2021-04-22 12:10:26 +0200

    Verify inOrder

--- Tag2_01PersonenDemo/src/main/java/de/gothaer/personendemo/services/PersonServiceImpl.java
+++ Tag2_01PersonenDemo/src/main/java/de/gothaer/personendemo/services/PersonServiceImpl.java
@@ -1,33 +1,69 @@
 package de.gothaer.personendemo.services;
 
+import java.util.List;
+
 import de.gothaer.personendemo.repositories.models.Person;
 import de.gothaer.personendemo.repositories.models.PersonRepository;
 
 public class PersonServiceImpl {
-	
-	private final PersonRepository repo;
 
-	public PersonServiceImpl(final PersonRepository repo) {
-		this.repo = repo;
-	}
+	private final PersonRepository repo;
+	private final List<String> antipathen;
+
 	
+
+	public PersonServiceImpl(final PersonRepository repo, final List<String> antipathen) {
+		
+		this.repo = repo;
+		this.antipathen = antipathen;
+	}
+
 	/*
-	 * Parameter darf nicht null sein -> PSE
-	 * Vorname darf nicht null sein und muss min 2. Zeichen -> PSE
-	 * Nachname darf nicht null sein und muss min 2. Zeichen -> PSE
+	 * Parameter darf nicht null sein -> PSE Vorname darf nicht null sein und muss
+	 * min 2. Zeichen -> PSE Nachname darf nicht null sein und muss min 2. Zeichen
+	 * -> PSE
 	 * 
 	 * Vorname darf nicht Attila sein -> PSE
 	 * 
-	 * Fehler in darunter liegenden Service fangen und in PSE wandeln (ab hier wird Mock benötigt) -> PSE
+	 * Fehler in darunter liegenden Service fangen und in PSE wandeln (ab hier wird
+	 * Mock benötigt) -> PSE
 	 * 
 	 * Speichern der Person im Repo
 	 * 
 	 */
 	public void speichern(final Person person) throws PersonServiceException {
-		if(person == null) 
-			throw new PersonServiceException("Parameter darf nicht null sein.");
+		try {
+			speichernImpl(person);
+
+		} catch (final RuntimeException e) {
+			throw new PersonServiceException("Unerwarteter Fehler in der Datenbank.");
+		}
+
 	}
-	
-	
+
+	private void speichernImpl(final Person person) throws PersonServiceException {
+		checkPerson(person);
+		repo.save(person);
+	}
+
+	private void checkPerson(final Person person) throws PersonServiceException {
+		validatePerson(person);
+		businessCheck(person);
+	}
+
+	private void businessCheck(final Person person) throws PersonServiceException {
+		if (antipathen.contains(person.getVorname()))
+			throw new PersonServiceException("Antipath.");
+	}
+
+	private void validatePerson(final Person person) throws PersonServiceException {
+		if (person == null)
+			throw new PersonServiceException("Parameter darf nicht null sein.");
+
+		if (person.getVorname() == null || person.getVorname().length() < 2)
+			throw new PersonServiceException("Vorname muss min. 2 Zeichen enthalten.");
+		if (person.getNachname() == null || person.getNachname().length() < 2)
+			throw new PersonServiceException("Nachname muss min. 2 Zeichen enthalten.");
+	}
 
 }
--- Tag2_01PersonenDemo/src/test/java/de/gothaer/personendemo/services/PersonServiceImplTest.java
+++ Tag2_01PersonenDemo/src/test/java/de/gothaer/personendemo/services/PersonServiceImplTest.java
@@ -2,26 +2,96 @@ package de.gothaer.personendemo.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import de.gothaer.personendemo.repositories.models.Person;
 import de.gothaer.personendemo.repositories.models.PersonRepository;
 
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceImplTest {
 	
+	@Mock private PersonRepository repoMock;
+	@Mock private List<String> antipathenMock;
 	@InjectMocks private PersonServiceImpl objectUnderTest;
-	@Mock private PersonRepository personRepositoryMock;
 	
+	
+	private final Person validPerson = new Person("John", "Doe");
+	
+	
+		
+	@Test
+	void speichern_PersonParameterIsNull_throwsPersonenServiceException() throws Exception{
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(null));
+		assertEquals("Parameter darf nicht null sein.", ex.getMessage());
+	} 
 
 	@Test
-	void speichern_PersonParameterIsNull_throwsPersonenServiceException() {
-		final PersonServiceException e = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(null));
-		assertEquals("Parameter darf nicht null sein.", e.getMessage());
+	void speichern_PersonVornameIsNull_throwsPersonenServiceException() throws Exception{
+		final Person person = new Person(null, "Doe");
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Vorname muss min. 2 Zeichen enthalten.", ex.getMessage());
 	}
+	@Test
+	void speichern_PersonVornameIsTooShort_throwsPersonenServiceException() throws Exception{
+		final Person person = new Person("J", "Doe");
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Vorname muss min. 2 Zeichen enthalten.", ex.getMessage());
+	}
+
+	@Test
+	void speichern_PersonNachnameIsNull_throwsPersonenServiceException() throws Exception{
+		final Person person = new Person("John", null);
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Nachname muss min. 2 Zeichen enthalten.", ex.getMessage());
+	}
+	@Test
+	void speichern_PersonNachnameIsTooShort_throwsPersonenServiceException() throws Exception{
+		final Person person = new Person("John", "D");
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Nachname muss min. 2 Zeichen enthalten.", ex.getMessage());
+		Mockito.verify(repoMock, Mockito.never()).save(Mockito.any(Person.class));
+	}
+
+	@Test
+	void speichern_Antipath_throwsPersonenServiceException() throws Exception{
+		when(antipathenMock.contains(Mockito.anyString())).thenReturn(true);
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(new Person("Attila","Doe")));
+		assertEquals("Antipath.", ex.getMessage());
+	}
+
+	@Test
+	void speichern_UnexpectedRuntimeExceptionInUnderlyingService_throwsPersonenServiceException() throws Exception{
+		
+		when(antipathenMock.contains(Mockito.anyString())).thenReturn(false);
+		when(repoMock.save(any(Person.class))).thenThrow(new ArithmeticException());
+				
+		final PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(validPerson));
+		assertEquals("Unerwarteter Fehler in der Datenbank.", ex.getMessage());
+	}
+
+	@Test
+	void speichern_HappyDay_PersonIsPassedToRepo() throws Exception{
+		when(antipathenMock.contains(Mockito.anyString())).thenReturn(false);
+		objectUnderTest.speichern(validPerson);
+		
+		final InOrder inOrder = Mockito.inOrder(antipathenMock, repoMock);
+		
+		inOrder.verify(antipathenMock,Mockito.times(1)).contains(Mockito.anyString());
+		inOrder.verify(repoMock, Mockito.times(1)).save(validPerson);
+		
+	}
+
+	
 }
